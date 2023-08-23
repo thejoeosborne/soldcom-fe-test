fe06b294490df7e87c1a631687d166182b34dd59
Joe Osborne <joerosborne@gmail.com>
2023-08-22 20:46:06 -0700

    background height

--- src/components/PartialBackground.tsx
+++ src/components/PartialBackground.tsx
@@ -1,5 +1,5 @@
 export default function PartialBackground() {
   return (
-    <div className="bg-dark h-[calc(130vh)] md:h-[calc(60vh)] w-full absolute top-0 z-[-1]"></div>
+    <div className="bg-dark h-[calc(130vh)] sm:h-[42rem] lg:h-[36rem] w-full absolute top-0 z-[-1]"></div>
   );
 }
